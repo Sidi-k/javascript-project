a48b069435b3df2ee7924ae0cb8cdf38b3dcb8cc
Said <Kasraie1988@gmail.com>
2022-09-21 13:22:45 +0200

    21.Sep

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ function deleteALL() {
   container.innerHTML = "";
 }
 
+const dark_mode_button=document.getElementById("dark_mode_button");
+dark_mode_button.addEventListener("click",()=>{
+  document.body.classList.toggle("dark_mode")
+})
